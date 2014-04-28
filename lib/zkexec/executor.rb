f6627be393e9a82326b67a4963ce04c6447f1178
Kyle Maxwell <kyle@kylemaxwell.com>
2014-04-28 12:57:08 -0700

    locks should respect health checks

--- lib/zkexec/executor.rb
+++ lib/zkexec/executor.rb
@@ -7,13 +7,14 @@ module ZkExec
     include Process
     
     def initialize(options)
-      @cmd         = options[:exec]
-      @cluster     = options[:cluster]
-      @health      = options[:health]
-      @mirrors     = options[:mirrors]
-      @alert       = options[:alert]
-      @alert_delay = options[:alert_delay]
-      @lock_name   = options[:lock]
+      @cmd             = options[:exec]
+      @cluster         = options[:cluster]
+      @health          = options[:health]
+      @health_interval = options[:health_interval]
+      @health_delay    = options[:health_delay]
+      @mirrors         = options[:mirrors]
+      @alert           = options[:alert]
+      @lock_name       = options[:lock]
       
       log "connecting to #{@cluster}"
       @zk = ZK.new(@cluster, :timeout => 1, :retry_duration => 10)
@@ -56,13 +57,13 @@ module ZkExec
     
     private
     def with_restart_lock
-      if @restart_lock
+      if @restart_lock && !@hold_lock_forever
         log "waiting on lock: #{@lock_name}"
         @restart_lock.lock!
       end
       @local_lock.synchronize { yield }
     ensure
-      if @restart_lock
+      if @restart_lock && !@hold_lock_forever
         @restart_lock.unlock!
         log "released lock: #{@lock_name}"
       end
@@ -72,6 +73,10 @@ module ZkExec
     def kill_to_refork
       if @child
         with_restart_lock do
+          if @health_checks
+            Thread.kill(@health_checks) 
+            @health_checks = nil
+          end
           @should_refork = true
           child = @child
           @child = nil
@@ -90,8 +95,44 @@ module ZkExec
           else
             log "#{child} terminated"
           end
+          
+          # wait for the other thread to bring the process back
+          health_checks_started = Time.now
+          while Time.now - health_checks_started < @health_delay
+            if system(@health)
+              @hold_lock_forever = false
+              return
+            end
+            sleep 1
+          end
+          
+          # not healthy, hang onto the lock forever, but still be
+          # responsive to config file changes
+          @hold_lock_forever = true
+          alert
         end
       end
+    end
+    
+    def alert 
+      if @alert
+        fork { exec(@alert) }
+      end
+    end
+    
+    def start_health_thread
+      @health_checks ||= @health && Thread.new {
+        sleep @health_delay
+        loop do
+          log "health checking via: #{@health}"
+          pid = fork { exec(@health) }
+          wait pid
+          if $?.exitstatus != 0
+            alert
+          end
+          sleep @health_interval
+        end
+      }
     end
 
     public
@@ -101,12 +142,19 @@ module ZkExec
 
     def execute
       @should_refork = true
+      
       while @should_refork
+        start_health_thread
         log "forking #{@cmd}"
         @child = fork { exec @cmd }
         log "forked #{@child}"
         @should_refork = false
         wait @child
+      end
+
+      if $?.exitstatus != 0
+        alert 
+        raise "command failed"
       end
     end
   end
--- lib/zkexec/runner.rb
+++ lib/zkexec/runner.rb
@@ -8,7 +8,7 @@ module ZkExec
     end
 
     def run(args)
-      options = { :mirrors => [], :cluster => "localhost:2181" }
+      options = { :mirrors => [], :cluster => "localhost:2181", :health_interval => 30, :health_delay => 30 }
       opts = OptionParser.new do |opts|
         opts.banner = "Usage: zkexec [options]\n\nRun a command, and restart if the config files change on the remote zookeeper.\n\n"
 
@@ -24,6 +24,14 @@ module ZkExec
           options[:health] = s
         end
         
+        opts.on("-i", "--health-interval INTERVAL", "Health-check every INTERVAL seconds") do |s|
+          options[:health_interval] = s.to_f
+        end
+        
+        opts.on("-d", "--health-delay INTERVAL", "Wait before starting health checks") do |s|
+          options[:health_delay] = s.to_f
+        end
+        
         opts.on("-m", "--mirror LOCAL_PATH=ZK_PATH", "Mirror a config file from zookeeper to localhost") do |s|
           options[:mirrors] << s.split("=")
         end
@@ -34,10 +42,6 @@ module ZkExec
                 
         opts.on("-a", "--alert COMMAND", "Run this command if the primary command returns","falsey or health checks fail for too long") do |s|
           options[:alert] = s
-        end
-        
-        opts.on("-d", "--alert-delay SECONDS", "How long to wait for health checks to succeed") do |s|
-          options[:alert_delay] = s.to_i
         end
         
         opts.on("-v", "--verbose") do
